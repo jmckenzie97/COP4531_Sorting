5177b9536a393f1c422ef8ce7fa4cbdbb13ff3fd
jmckenzie97 <49374308+jmckenzie97@users.noreply.github.com>
2020-03-06 13:35:55 -0500

    STArted heap sort

--- heap_sort.cpp
+++ heap_sort.cpp
@@ -2,3 +2,36 @@
 // Created by Justin on 2/24/2020.
 //
 
+#include <iostream>
+using namespace std;
+void heapify(int *arr, int len, int i)
+{
+    int largest = i;
+    int left = 2 * i + 1;
+    int right = 2 * i + 2;
+
+    if (right < len && arr[right] > arr[largest])
+        largest = right;
+
+    if (left < len && arr[left] > arr[largest])
+        largest = left;
+
+    if (largest != i)
+    {
+        swap(arr[i], arr[largest]);
+
+        heapify(arr, len, largest);
+    }
+}
+
+void heapSort(int *arr, int len)
+{
+    for (int i = len / 2 - 1; i >= 0; i--)
+        heapify(arr, len, i);
+
+    for (int i = len - 1; i >= 0; i--)
+    {
+        swap(arr[0], arr[i]);
+        heapify(arr, i, 0);
+    }
+}
